1e2ed9e81055dba1c98bb5f9a4ddbe1ecc83d513
noahmagcalas <noah.magcalas@gmail.com>
2022-10-01 00:01:23 -0500

    all go to mid

--- src/main/java/starterpack/strategy/RushMidStrategy.java
+++ src/main/java/starterpack/strategy/RushMidStrategy.java
@@ -5,6 +5,7 @@ import starterpack.game.CharacterClass;
 import starterpack.game.Item;
 import starterpack.game.Position;
 import starterpack.util.Logger;
+import starterpack.util.Utility;
 
 public class RushMidStrategy implements Strategy {
 
@@ -24,16 +25,32 @@ public class RushMidStrategy implements Strategy {
      * @return
      */
     public Position moveActionDecision(GameState gameState, int myPlayerIndex) {
+
         Position playerPos = gameState.getPlayerStateByIndex(myPlayerIndex).getPosition();
         int playerX = playerPos.getX();
         int playerY = playerPos.getY();
+        int moveX = 0;
+        int moveY = 0;
+        boolean notInMid = !((playerX == 4 || playerX == 5) && (playerY == 4 || playerY == 5));
 
         if (myPlayerIndex == 0) {
-            if (!((playerX == 5 || playerX == 6) && (playerY == 5 || playerY == 6))) {
-                return new Position(playerPos.getX()+1, playerPos.getY()+1);
+            if (notInMid) {
+                return new Position(playerX + 1, playerY + 1);
+            }
+        } else if (myPlayerIndex == 1) {
+            if (notInMid) {
+                return new Position(playerX + -1, playerY + 1);
+            }
+        } else if (myPlayerIndex == 2) {
+            if (notInMid) {
+                return new Position(playerX - 1, playerY - 1);
+            }
+        } else if (myPlayerIndex == 3) {
+            if (notInMid) {
+                return new Position(playerX + 1, playerY - 1);
             }
         }
-        return gameState.getPlayerStateByIndex(myPlayerIndex).getPosition();
+        return playerPos;
     }
 
     /**
@@ -43,7 +60,20 @@ public class RushMidStrategy implements Strategy {
      * @return
      */
     public int attackActionDecision(GameState gameState, int myPlayerIndex) {
-        return 0;
+        int res = 0;
+        for (int i = 0; i < 4; i++) {
+
+            if (i != myPlayerIndex) {
+                if (Utility.chebyshevDistance(
+                        gameState.getPlayerStateByIndex(myPlayerIndex).getPosition(),
+                        gameState.getPlayerStateByIndex(i).getPosition()) <=
+                        gameState.getPlayerStateByIndex(myPlayerIndex).getCharacterClass().getStatSet().getRange()) {
+                    return i;
+                }
+                res = i;
+            }
+        }
+        return res;
     }
 
     /**
