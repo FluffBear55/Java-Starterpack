99a13e8e2a9355ba6e580eb74fdb441abec88643
noahmagcalas <noah.magcalas@gmail.com>
2022-09-30 22:51:02 -0500

    sdahfuabsdf

--- src/main/java/starterpack/strategy/RushMidStrategy.java
+++ src/main/java/starterpack/strategy/RushMidStrategy.java
@@ -5,7 +5,7 @@ import starterpack.game.CharacterClass;
 import starterpack.game.Item;
 import starterpack.game.Position;
 
-public class StarterStrategy implements Strategy {
+public class RushMidStrategy implements Strategy {
 
     /**
      * When the game initializes, you need to decide your starting class!
@@ -22,7 +22,13 @@ public class StarterStrategy implements Strategy {
      * @param myPlayerIndex
      * @return
      */
-    public Position moveActionDecision(GameState gameState, int myPlayerIndex) { return new Position(0, 0); }
+    public Position moveActionDecision(GameState gameState, int myPlayerIndex) {
+        Position playerPos = gameState.getPlayerStateByIndex(myPlayerIndex).getPosition();
+        if (myPlayerIndex == 0) {
+            return new Position(playerPos.getX()+1, playerPos.getY()+1);
+        }
+        else return gameState.getPlayerStateByIndex(myPlayerIndex).getPosition();
+    }
 
     /**
      *
